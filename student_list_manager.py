fdbf230e7e21616ac5aee2cfe5b442e0d662c599
Amrit Ray <amritray43@gmail.com>
2026-06-20 23:31:46 +0530

    Update source code files

--- student_list_manager.py
+++ student_list_manager.py
@@ -1,22 +1,31 @@
 def add_student():
     name = (input("Enter student's name: "))
-    students.append(name)
+    students.append(name.lower())
 
 def view_students():
     for student in students:
         print(student)
 
+def search_student():
+    search = input("Enter a name to search: ").lower()
+    if search in students:
+        print(f"Found at No.{students.index(search)}")
+    else:
+        print("Not Found")
+
 students=[]
 
 while True:
-    print("Enter 1 to add student\nEnter 2 to view students\nEnter 3 to exit")
+    print("\nEnter 1 to add student\nEnter 2 to view students\nEnter 3 to search a student\nEnter 4 to exit\n")
     num= (input("Enter a number: "))
     if num == '1':
         add_student()
     elif num == '2':
         view_students()
-    elif num == '3':
+    elif num =='3':
+        search_student()
+
+    elif num == '4':
         break
     else:
         print("Invalid input")
-        break
