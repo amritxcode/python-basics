ebd44e91b1c19d8dfeb0a34e0b1a1dd665a12060
Amrit Ray <amritray43@gmail.com>
2026-06-21 00:04:23 +0530

    Add second largest number in the list code

---
+++ secondLargest_number_in_the_list.py
@@ -0,0 +1,13 @@
+def largest_number(nums):
+    largest = nums[0]
+    sec_largest= float('-inf')
+    for num in nums:
+        if num > largest:
+            sec_largest = largest
+            largest = num
+        elif num < largest and num > sec_largest:
+            sec_largest = num
+
+    return sec_largest 
+
+print(largest_number([3, 9, 2, 15, 7]))
