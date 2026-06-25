c9f2307ff16a125befe9b7c9c910f7cd3604975c
Amrit Ray <amritray43@gmail.com>
2026-06-25 23:18:12 +0530

    Add update student feature

--- student_management_system.py
+++ student_management_system.py
@@ -47,6 +47,68 @@ def search_student():
     search_id = int(input("Enter student id: "))
     for student in students:
         if search_id == student["id"]:
-            print(f"ID: {student["id"]}, Name: {student["name"]}, Age: {student["age"]}, CGPA: {student["cgpa"]}")
+            print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, CGPA: {student['cgpa']}")
             return
+    print("Student not found")
+
+def delete_student():
+    student_id = int(input("Enter student ID: "))
+    for student in students:
+        if student_id == student["id"]:
+            print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, CGPA: {student['cgpa']}")
+            while True:
+                ask = input("Are you sure to delete this student(y/n): ").lower()
+                if ask == "y":
+                    students.remove(student)
+                    print(f"ID number {student_id} successfully deleted")
+                    return
+                elif ask == "n":
+                    print("No data was deleted.")
+                    return
+                else:
+                    print("Please choose 'y' or 'n'")
+
+    print("Student not found.")
+
+
+def update_student():
+    student_id = int(input("Enter student ID: "))
+    for student in students:
+        if student_id == student["id"]:
+    
+            update = int(input("Enter 1. to update name\nEnter 2. to update age\nEnter 3. to update CGPA: "))
+            if update == 1:
+                for student in students:
+                    if student_id == student["id"]:
+                        print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, CGPA: {student['cgpa']}")
+                        newName = input("Enter new name: ")
+                        
+                        student["name"] = newName
+
+                        print("Name updated successfully!.")
+                        return
+            elif update == 2:
+                for student in students:
+                    if student_id == student["id"]:
+                        print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, CGPA: {student['cgpa']}")
+                        newAge = int(input("Enter new age: "))
+
+                        student["age"] = newAge
+
+                        print("Age updated successfully!.")
+                        return
+                
+            elif update == 3:
+                for student in students:
+                    if student_id == student["id"]:
+                        print(f"ID: {student['id']}, Name: {student['name']}, Age: {student['age']}, CGPA: {student['cgpa']}")
+                        newCgpa = float(input("Enter new CGPA: "))
+                        
+                        student["cgpa"] = newCgpa
+
+                        print("CGPA updated successfully!.")
+                        return
+            else:
+                print("Invalid number, choose again")
+                return
     print("Student not found")
