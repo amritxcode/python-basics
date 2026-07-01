6248e4a8790de58b797bd0cd4f43658124d7eac6
Amrit Ray <amritray43@gmail.com>
2026-07-02 00:13:59 +0530

    Add Python basics exercises

--- find_max.py
+++ find_max.py
@@ -1,6 +1,9 @@
 def find_max(*numbers):
+    if not numbers:
+        return None
+    
     largest = float('-inf')
     for num in numbers:
-        if num >= largest:
+        if num > largest:
             largest = num
     return largest
