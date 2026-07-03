52e4c55c67b345ca62cc6fbef35a3655bd832bd6
Amrit Ray <amritray43@gmail.com>
2026-07-03 12:29:45 +0530

    Add file: kwargs.py

---
+++ kwargs.py
@@ -0,0 +1,14 @@
+def print_student(**details):
+    """
+    
+    """
+    student_info = {}
+    for key, value in details.items():
+        return details
+
+print(print_student(
+    name="Amrit",
+    age=21,
+    branch="AI & ML",
+    college="XYZ"
+))
