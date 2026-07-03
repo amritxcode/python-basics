e5953085ccfb07848d03c998c735181eb6b15eb4
Amrit Ray <amritray43@gmail.com>
2026-07-03 12:29:50 +0530

    Add file: secondLargest_number_in_the_list.py

---
+++ secondLargest_number_in_the_list.py
@@ -0,0 +1,13 @@
+def secondlargest_number(nums):
+    largest = nums[0]
+    sec_largest= float('-inf')
+    for num in nums:
+        if num > largest:
+            sec_largest = largest
+            largest = num
+        elif num < largest and num > sec_largest:
+            sec_largest = num
+
+    return sec_largest 
+
+print(secondlargest_number([10,10,5]))
