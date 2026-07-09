7f352e1a156ded935bc63833473e593915c28bed
Amrit Ray <amritray43@gmail.com>
2026-07-09 16:57:07 +0530

    Update buzz_fizz

--- buzzfizz_2_3_5.py
+++ buzzfizz_2_3_5.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 num = 2
 
 for i in range (1,21):
@@ -8,15 +7,4 @@ for i in range (1,21):
     elif temp%3 ==0:
         print("buzzfizz")
     elif temp%2==0:
-=======
-num = 2
-
-for i in range (1,21):
-    temp=i*2
-    if temp% 2 ==0 and temp %3 ==0 and temp%5 == 0:
-        print("Maka")
-    elif temp%3 ==0:
-        print("buzzfizz")
-    elif temp%2==0:
->>>>>>> 6d072e47ce2fd69e156d0c5c1649bf8192d9a9d1
         print("buzz")
