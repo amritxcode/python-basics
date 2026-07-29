9edd8eb3ae639a41abc0c4bb1195040a5736f9c5
Amrit Ray <amritray43@gmail.com>
2026-07-29 23:45:12 +0530

    Add: string basics

---
+++ string_basics.py
@@ -0,0 +1,10 @@
+s = 'hello world'
+
+print(s.upper())
+print(s.split())
+print(s.replace('l','L'))
+print(s[::-1])
+print(s.count('l'))
+print('_'.join(['a','b','c','d']))
+print(len(s))
+print(s[2:5])
