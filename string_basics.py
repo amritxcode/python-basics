5b14d86dd6ada258f2b0679369677edb515d8e0a
Amrit Ray <amritray43@gmail.com>
2026-07-30 01:37:04 +0530

    Add: string basics

--- string_basics.py
+++ string_basics.py
@@ -1,14 +1,10 @@
-# s = 'hello world'
+s = 'hello world'
 
-# print(s.upper())
-# print(s.split())
-# print(s.replace('l','L'))
-# print(s[::-1])
-# print(s.count('l'))
-# print('_'.join(['a','b','c','d']))
-# print(len(s))
-# print(s[2:5])
-
-x = [1,2,2,2,3,4,1,4,5,22,43,5,2]
-x = set(x)
-print(x)
+print(s.upper())
+print(s.split())
+print(s.replace('l','L'))
+print(s[::-1])
+print(s.count('l'))
+print('_'.join(['a','b','c','d']))
+print(len(s))
+print(s[2:5])
